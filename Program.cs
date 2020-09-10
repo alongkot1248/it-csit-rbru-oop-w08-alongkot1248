7d739daf0167e8dd8f418127ac1dc28027e865e9
alongkot1248 <62496304+alongkot1248@users.noreply.github.com>
2020-09-11 00:26:25 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace oop_6115261031_w08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Img i1 = new Img("sea", "500","300", "321112", "JPG");
+            Album a1 = new Album("001", "view", "321112", "img",  i1);
+            Console.WriteLine(a1.ToString());
+            Img i2 = new Img("waterfall", "650", "700", "112111", "JPG");
+            Album a2 = new Album("002", "view", "112111", "img", i2);
+            Console.WriteLine(a2.ToString());
+        }
+    }
+}
